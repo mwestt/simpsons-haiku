e2e16d12ea90db61ba393c8ad76b9a4950eaef7a
West, Matthew <mwest8@mgh.harvard.edu>
2022-10-01 19:00:30 -0400

    GCP GitHub integration test

--- main.py
+++ main.py
@@ -1,41 +1,44 @@
-import os
-import json
+# import os
+# import json
+# import tweepy
+# import compuglobal
+# import requests
 import numpy as np
-from datetime import datetime
+# from datetime import datetime
 
-from twitter import SimpsonsTwitterBot
+# from haiku import SimpsonsHaiku
+
 
 def main():
+    print('Starting point')
+    # try:
+    #     # Local version
+    #     auth_dict=json.load(open('auth.json'))
+    # except:
+    #     # GCP version
+    #     auth_dict = {
+    #         'api_key': os.environ.get("CONSUMER_KEY"),
+    #         'api_key_secret': os.environ.get("CONSUMER_SECRET"),
+    #         'access_token': os.environ.get("ACCESS_TOKEN"),
+    #         'access_token_secret': os.environ.get("ACCESS_TOKEN_SECRET")
+    #     }
 
-    try:
-        # Local version
-        auth_dict=json.load(open('auth.json'))
-    except:
-        # GCP version
-        auth_dict = {
-            'api_key': os.environ.get("CONSUMER_KEY"),
-            'api_key_secret': os.environ.get("CONSUMER_SECRET"),
-            'access_token': os.environ.get("ACCESS_TOKEN"),
-            'access_token_secret': os.environ.get("ACCESS_TOKEN_SECRET")
-        }
+    # simpsons_bot = SimpsonsTwitterBot(
+    #     auth_dict=auth_dict,
+    #     haiku_df='haiku_df.csv'
+    # )
 
-    simpsons_bot = SimpsonsTwitterBot(
-        auth_dict=auth_dict,
-        haiku_df='haiku_df.csv'
-    )
+    # # Select media type at random
+    # media_type = np.random.choice(['jpg', 'gif'])
 
-    # Select media type at random
-    media_type = np.random.choice(['jpg', 'gif'])
+    # # "Golden-age Wednesdays"
+    # day = datetime.today().weekday()
+    # golden_age = True if day == 2 else False
 
-    # "Golden-age Wednesdays"
-    day = datetime.today().weekday()
-    golden_age = True if day == 2 else False
-
-    # Tweet on, son, tweet on!
-    print(simpsons_bot.tweet_haiku(media_reply=True, media_type=media_type, 
-                                   add_metadata=True, golden_age=golden_age))
+    # # Tweet on, son, tweet on!
+    # simpsons_bot.tweet_haiku(media_reply=True, media_type=media_type, 
+    #                          add_metadata=True, golden_age=golden_age)
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
